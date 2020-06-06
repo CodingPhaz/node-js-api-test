1d4e9fe18d2e461faf6c68d1021b19800cb3c9b9
Samuel Bouye <samuel@arch-linux.localdomain>
2020-06-06 17:52:21 +0000

    add mysql db

---
+++ models/connection.js
@@ -0,0 +1,17 @@
+
+
+var mysql = require('mysql')
+
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:''
+})
+
+con.connect(err=>{
+    if(err)throw err;
+    console.log('Db connected')
+})
+
+exports.modules = con
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
+const con = require('./models/connection')
 
-app.get('/', (req,res) => (
+app.get('/',(req,res)=>{
     res.send('Hello world')
-))
-app.get('/about', (req,res) => (
-    res.send('About')
-))
+})
+
 
 app.listen(3000,console.log('Port listen in 3000'))
