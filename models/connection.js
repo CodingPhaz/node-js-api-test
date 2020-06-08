ad076d5db46c6983ff6b1b48947eb8e04ab2b48f
Samuel Bouye <bouyesamuel@gmail.com>
2020-06-08 16:14:31 +0000

    configurate and display data from database

--- models/connection.js
+++ models/connection.js
@@ -1,23 +1,54 @@
+const Sequelize = require('sequelize')
 
-
-var mysql = require('mysql')
-
-
-var con = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database: 'testdbnode'
+const sequelize = new Sequelize('testdbnode', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mariadb'
 })
 
-con.connect(err=>{
-    if(err)throw err;
-    console.log('Db connected')
- var sql = "select * from chats"
-    con.query(sql, (err,result,fields   )=> {
-        if(err) throw err;
-        console.log(result)
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection is OK')
     })
+    .catch(err => {
+        console.log('Unable to connect to database : ', err)
+    })
+
+
+
+const Model = Sequelize.Model;
+class Chat extends Model{}
+
+Chat.init({
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    message: {
+        type: Sequelize.STRING
+    }
+}, {
+    sequelize,
+    modelName: 'chat'
+}
+)
+
+
+Chat.findAll().then(chat => {
+    var ok = JSON.stringify(chat,null,4);
+    var result = JSON.parse(ok)
+    for(const x in result){
+        console.log(`Nom : ${result[x].name} 
+        Message : ${result[x].message}`)
+    }
+    // for(var i = 0; i < ok.name;  i++ ){
+        // console.log(ok["id"])
+    // }
+    // console.log('All message in chat : ', JSON.stringify(chat,null, 2))
+    let chatt = JSON.stringify(chat,null, 4)
+    module.exports.chatted = JSON.parse(chatt)
 })
 
-// exports.modules = con
+// Chat.create({ name: 'Sophonie', message: 'Salut tout marche parfaitement'}).then(sam => {
+//     console.log('Your id is ', sam.id)
+// })
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const app = express()
-const con = require('./models/connection')
+const chatted = require('./models/connection')
+const path = require('path')
+var bodyParser = require('body-parser')
+let ejs = require('ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+// app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
 
 app.get('/',(req,res)=>{
-    res.send('Hello world')
+    // for(chat in con.chat){
+        let chatting = chatted.chatted
+        // req.body(con.chatt)
+        // res.send(con.chatt)
+        // let html = ejs.render('<%= chatting; %>', {chatting: chatting});
+
+        res.render('index.ejs', {chatting : chatting})
+        // res.send(chatting)
+        // app.render('views/index', { chatting: 'result' })
+    // }
+
+})
+
+app.post('/newPost', (req,res)=> {
+    console.dir(req.body);
+    res.send(req.body)
+    console.log(req.body)
 })
 
 
