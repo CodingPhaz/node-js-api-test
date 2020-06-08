8cd9a282137d31eb042e98947b0d0e5f697463e2
Samuel Bouye <samuel@arch-linux.localdomain>
2020-06-08 11:51:40 +0000

    connect to db mysql

--- models/connection.js
+++ models/connection.js
@@ -6,12 +6,18 @@ var mysql = require('mysql')
 var con = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:''
+    password:'root',
+    database: 'testdbnode'
 })
 
 con.connect(err=>{
     if(err)throw err;
     console.log('Db connected')
+ var sql = "select * from chats"
+    con.query(sql, (err,result,fields   )=> {
+        if(err) throw err;
+        console.log(result)
+    })
 })
 
-exports.modules = con
+// exports.modules = con
