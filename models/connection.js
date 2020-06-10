65d3c0198e9cc31b535567670ff3d83332863ce4
Samuel Bouye <bouyesamuel@gmail.com>
2020-06-10 16:59:52 +0000

    refactorize code

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('testdbnode', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mariadb'
+    dialect: 'mariadb',
+    pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 })
-// const post = require('../server.js')
 
 
 sequelize.authenticate()
@@ -16,46 +21,38 @@ sequelize.authenticate()
 
 
 
-const Model = Sequelize.Model;
-class Chat extends Model{}
+    const Model = Sequelize.Model;
+    class Chat extends Model{}
 
-Chat.init({
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    message: {
-        type: Sequelize.STRING
+
+
+
+    Chat.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        message: {
+            type: Sequelize.STRING
+        }
+    }, {
+        sequelize,
+        modelName: 'chat'
     }
-}, {
-    sequelize,
-    modelName: 'chat'
-}
-)
+    )
 
-//Afficher les donnee
-// var chatted;
-Chat.findAll().then(chat => {
-    var getData = JSON.stringify(chat,null,4);
-    // var parseResult = JSON.parse(getData)
-    // for(const getOne in parseResult){
-        // console.log(`Nom : ${parseResult[getOne].name} 
-        // Message : ${parseResult[getOne].message}`)
-    // }
+    //Afficher les donnee
+    Chat.findAll()
+        .then(chat => {
+            var getData = JSON.stringify(chat,null,4);
+                module.exports.chatParsed = JSON.parse(getData)
+       })
 
-    // let chatt = JSON.stringify(chat,null, 4)
- module.exports.chatParsed = JSON.parse(getData)
-})
 
 
 let newPost = function newChat(post){
     return Chat.create(post)
 }
 
-
-//Ajout
-// console.log('nouveaux post ajouter ',post)
-
-//  chatted
 module.exports.newMessage= newPost
 
--- server.js
+++ server.js
@@ -1,53 +1,32 @@
 const express = require('express')
 const app = express()
 const db = require('./models/connection')
-// const = require('./models/connection')
 var bodyParser = require('body-parser')
 let ejs = require('ejs');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-// app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
 
 app.get('/',(req,res)=>{
-    // for(chat in con.chat){
         let chatting = db.chatParsed
-        // req.body(con.chatt)
-        // res.send(con.chatt)
-        // let html = ejs.render('<%= chatting; %>', {chatting: chatting});
-
         res.render('index.ejs', {chatting : chatting})
-        // res.send(chatting)
-        // app.render('views/index', { chatting: 'result' })
-    // }
 
 })
 
 app.post('/newPost', (req,res)=> {
-    // console.dir(req.body);
     console.log(req.body)
     db.newMessage({name: req.body.name, message: req.body.message})
     .then(nM => {
-        console.log('message added', nM.name)
+            console.log('message added', nM.name)
     })
+    // .then(nM => {
+    // })
+    res.redirect('/')
     console.log(db.chatParsed)
-    // res.redirect('/')
-    // Chat.create({ name: req.body.name, message: req.body.message}).then(newM=> {
-        // console.log(newpost)
 })
 
 
 var server = app.listen(3003 ,  console.log('Port listen in 3000'))
 
-
-server.on('request', (req, res) => {
-   if (req.method === 'POST') {
-        collectRequestData(req, res => {
-            console.log(res);
-        });
-   } 
-   // Here the index.html should be reloaded
- res.redirect('/'); // --->Redirect to index here.
-});
