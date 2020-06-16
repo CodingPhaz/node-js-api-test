8808ee56ee2b1c1ce137537df25160fe2ef4e84f
Samuel Bouye <bouyesamuel@gmail.com>
2020-06-16 10:46:23 +0000

    delete feature... get id of message selected

--- models/connection.js
+++ models/connection.js
@@ -18,7 +18,7 @@ sequelize.authenticate()
         console.log('Unable to connect to database : ', err)
     })
 
-
+    //Initialiser le model a la base de donnee
     const Chat = sequelize.define('chat',{
         name: {
             type: Sequelize.STRING,
@@ -29,7 +29,7 @@ sequelize.authenticate()
         }
     });
     
-
+    //afficher la page d'acceuil
     router.get('/',(req,res) => {
         res.send('home pagee')
     })
@@ -48,9 +48,17 @@ sequelize.authenticate()
         })
     })
 
+
+    router.get('/delete/:id', (req,res) => {
+        res.send(req.params)
+    })
+
+    //le formulaire d'ajout de nouveaux messsage
     router.get('/add', (req,res)=>{
         res.render('index.ejs')
     })
+
+
     // Cree de nouveaux message
     router.post('/newPost', (req, res) => {
         Chat.create({name: req.body.name, message: req.body.message})
@@ -58,6 +66,7 @@ sequelize.authenticate()
                 console.log(chatBox)
         })
 
+        //rediriger apres l'ajout d'un nouveau message
         res.redirect('/post');
         // console.log(db.chatParsed)
 
